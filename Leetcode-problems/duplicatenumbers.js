1d7d639e7a5a06ec9b39e1094888b9215757f4db
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-01 21:36:19 -0400

    add better solution to js duplicate numbers

--- Leetcode-problems/duplicatenumbers.js
+++ Leetcode-problems/duplicatenumbers.js
@@ -1,13 +1,30 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+// var containsDuplicate = function(nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//             for (let j = 0; j < nums.length; j++) {
+//                 if (i != j && nums[i] == nums[j])
+//                     return true;
+//             }
+//         }
+//     return false;
+// };
+
+// better solution
+
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-            for (let j = 0; j < nums.length; j++) {
-                if (i != j && nums[i] == nums[j])
-                    return true;
-            }
-        }
-    return false;
+    let uniqueNums = new Set();
+   for (let num of nums) {
+       if (uniqueNums.has(num)) {
+           return true;
+       }
+       uniqueNums.add(num);
+   }
+   return false;
 };
