78cd275223a9c70194f36f9cf671b7cf6d424609
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-01 21:31:09 -0400

    fix C# contains duplicates solution

--- C-Sharp-Leetcode/containsDuplicate.cs
+++ C-Sharp-Leetcode/containsDuplicate.cs
@@ -1,13 +1,34 @@
+// public class Solution {
+//     public bool ContainsDuplicate(int[] nums) {
+//         for (int i = 0; i < nums.Length; i++)
+//         {
+//             for (int j = 0; j < nums.Length; j++)
+//             {
+//                 if (i != j && nums[i] == nums[j])
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+// top solution not accepted because time limit exceeded
+
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        for (int i = 0; i < nums.Length; i++)
+        HashSet<int> seen = new HashSet<int>();
+        foreach (int num in nums)
         {
-            for (int j = 0; j < nums.Length; j++)
+            if (seen.Contains(num))
             {
-                if (i != j && nums[i] == nums[j])
-                    return true;
+                return true;
             }
+            seen.Add(num);
         }
         return false;
     }
 }
+
+//  for a HashSet, imagine a magical room where you just shout the name of a toy, 
+//  and the room instantly tells you if the toy is there or not. You don't care 
+//  where the toy is; you just want to know if it's in the room.
