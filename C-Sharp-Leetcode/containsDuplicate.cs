f003ab4500a2c6b6be8cff4c3aaf1e39a903cfd2
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-01 21:20:12 -0400

    solve contains duplicates C# and JS

---
+++ C-Sharp-Leetcode/containsDuplicate.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (i != j && nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Leetcode-problems/duplicatenumbers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+            for (let j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] == nums[j])
+                    return true;
+            }
+        }
+    return false;
+};
