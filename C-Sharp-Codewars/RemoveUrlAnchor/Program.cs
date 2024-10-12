42a5808b8f8c76115dd319b24407fc778df7ae9d
Ellie <ellieslife@icloud.com>
2024-10-12 17:51:47 -0300

    .

---
+++ C-Sharp-Codewars/RemoveUrlAnchor/Program.cs
@@ -0,0 +1,11 @@
+﻿string RemoveUrlAnchor(string url)
+{
+    // Split the URL at the '#' character
+    string[] split = url.Split('#');
+
+    // Return the first part of the split array, which is the URL without the anchor
+    return split[0];
+}
+
+Console.WriteLine(RemoveUrlAnchor("www.meowmeowmewo.com/cheese#fjoandoas"));
+
